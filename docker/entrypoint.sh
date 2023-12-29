e78b5ae014040d484ebd059f6d59314bf132fa4f
Kenny Tat <kennytat.app@gmail.com>
2023-12-29 09:54:24 +0700

    feat: added comfyUI

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+
+## Start comfyUI
+cd /content/ComfyUI && python main.py --port 8188 --listen 0.0.0.0 &
+## Start Stable Diffusion
 cd /content/stable-diffusion-webui && python webui.py "$@"
